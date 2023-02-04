ce8c5ad7c0e1d5d1799a4773241d04603fec9be5
AbdurRehman-coder <abdurrehmanlkl786@gmail.com>
2023-02-05 01:04:27 +0500

    first server completed

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
+ // Initialization
 const express = require('express');
 const app = express();
 
+// Defined App Routes
 app.get('/', function(req, res){
-res.send('welcome to the home page')
+res.send('welcome to the home page');
+});
+
+app.get('/notes', (req, res) => {
+res.send('Welcome to Notes page');
 })
 
+// Starting the server on PORT
 app.listen(5000, function(){
     console.log('listening on server PORT: 5000');
 })
