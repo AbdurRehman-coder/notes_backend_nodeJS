559ff6dee1adbb9b9c8297f0e0575481d4799a8b
AbdurRehman-coder <abdurrehmanlkl786@gmail.com>
2023-02-07 21:19:13 +0500

    documentation about how to init a project

--- server.js
+++ server.js
@@ -15,3 +15,8 @@ res.send('Welcome to Notes page');
 app.listen(5000, function(){
     console.log('listening on server PORT: 5000');
 })
+
+// step1: initialize npm in the project to install other packages in libraries in the folder.
+// > npm init
+// Now to use express, install the express package
+// > npm install express
